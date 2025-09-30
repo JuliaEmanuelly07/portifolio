1a121656041cd508a03cab7296d97a7e724ce290
JuliaEmanuelly07 <juliaemanuellydasilvacosta@gmail.com>
2025-09-30 15:09:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,6 @@ def python_projects():
     return render_template("index.html")  
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=false)
+
