1bcefa09281aa7a3de687bf708bc37961c773ef3
JuliaEmanuelly07 <juliaemanuellydasilvacosta@gmail.com>
2025-10-07 16:50:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,41 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Rota principal
+# Dados para passar às páginas
+dados = {
+    
+    "nome": "Julia Emanuelly da Silva Costa",
+    "bio": """Oi! Eu sou a Júlia Emanuelly da Silva Costa, tenho 18 anos e gosto de aprender coisas novas todos os dias.
+Gosto de criar, testar ideias e ver as coisas acontecerem na prática. Cada projeto é uma oportunidade de fazer diferente e melhorar um pouco mais.Estou começando uma nova fase e quero aproveitar cada oportunidade para aprender e criar coisas do meu jeito.""",
+    "educacao": [
+        {"ano": "2022 - 2024", "curso": "Ensino Médio Técnico em Energias Renováveis", "instituicao": "Senai - Civit"},
+        {"ano": "2022", "curso": "Curso de AutoCAD", "instituicao": "Senai - Civit"},
+        {"ano": "2024 - 2026", "curso": "Desenvolvimento de Sistemas", "instituicao": "Senai - Civit"},
+        {"ano": "2025 - 2030", "curso": "Direito", "instituicao": "Faculdade Multivix"}
+    ],
+    "projetos": [
+        {"titulo": "Sistema de Login", "descricao": "Um sistema de login para autenticação de usuários.", "imagem": "login.jpg"},
+        {"titulo": "Implementação BFS", "descricao": "Algoritmo de busca em largura (BFS).", "imagem": "bfs.jpg"},
+        {"titulo": "Cenário", "descricao": "Atividade de criação de cenário com elementos gráficos variados.", "imagem": "cenario.jpg"},
+        {"titulo": "Jogo de Sinuca", "descricao": "Jogo de sinuca desenvolvido no Unity.", "imagem": "sinuca.jpg"}
+    ]
+}
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", dados=dados)
 
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html", dados=dados)
 
-@app.route("/python")
-def python_projects():
-    return render_template("index.html")  
+@app.route("/projetos")
+def projetos():
+    return render_template("projetos.html", dados=dados)
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html", dados=dados)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=false)
-
+    app.run(debug=True)
